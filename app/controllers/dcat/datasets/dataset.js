0d254de01389743f8524f8d8ec53b5c3b1f716e3
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-07-27 16:33:09 +0200

    feat: can download the odrl-policy triples when available
    Refs: lbron-1709

---
+++ app/controllers/dcat/datasets/dataset.js
@@ -0,0 +1,33 @@
+import Controller from '@ember/controller';
+
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+export default class DcatDatasetsDatasetController extends Controller {
+  @service toaster;
+
+  @action
+  async downloadODRLTriplesAsText() {
+    const odrlPolicyTtlResult = await fetch(
+      `/dcat/dataset/${this.model.id}/policy/ttl`,
+    );
+    const odrlPolicy = await odrlPolicyTtlResult.json();
+    const odrlPolicyTtl = odrlPolicy?.ttl;
+
+    if (!odrlPolicyTtl) {
+      this.toaster.error('Could not find a ODRL-policy', undefined, {
+        timeOut: 5000,
+      });
+      return;
+    }
+
+    let downloadLink = document.createElement('a');
+    downloadLink.download = 'odrl-policy-triples.ttl';
+
+    let blob = new Blob([odrlPolicyTtl], { type: 'text/turtle' });
+    downloadLink.href = window.URL.createObjectURL(blob);
+
+    downloadLink.click();
+    downloadLink.remove();
+  }
+}
