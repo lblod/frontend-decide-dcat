936abaadd24c42878a129fb2ca7965b294dee6e4
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-08-14 10:14:22 +0200

    feat: also support the language string
    Refs: lbron-1884

--- app/models/agent.js
+++ app/models/agent.js
@@ -6,6 +6,15 @@ export default class AgentModel extends Model {
   @attr email;
 
   get label() {
+    if (
+      Array.isArray(this.name) &&
+      this.name.length >= 1 &&
+      this.name[0].content.trim() != ''
+    ) {
+      // If language-string-set is set we show the first value e.g "name"@nl
+      return this.name[0].content;
+    }
+
     if (this.name && this.name.trim?.() != '') {
       return this.name;
     }
