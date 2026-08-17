a91dc7dc1f89a1ecf1431447627dd648568aa707
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-08-17 13:32:59 +0200

    fix: missing endpointurl property on dataservice

--- app/models/data-service.js
+++ app/models/data-service.js
@@ -4,6 +4,7 @@ export default class DataServiceModel extends Model {
   @attr uri;
   @attr('string') title;
   @attr('string') description;
+  @attr('string') endpointUrl;
   @attr('datetime') issued;
   @attr('datetime') modified;
 
